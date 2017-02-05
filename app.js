453f00b7a96d02531226ca67ba3867bb97baec8b
Fahad Murtaza <fahdim@gmail.com>
2017-02-06 04:31:50 +0500

    ESLinting\!

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
+const express = require('express');
+const path = require('path');
+// const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const config = require('./config');
-
-const mongooseConnected = mongoose.createConnection(`${config.get('database.host')}/${config.get('database.name')}}`, {
-  useMongoClient: true
+const connString = `${config.get('database.host')}/${config.get('database.name')}`;
+const mongooseConnected = mongoose.createConnection(connString, {
+  useMongoClient: true,
 });
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,7 +31,7 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
